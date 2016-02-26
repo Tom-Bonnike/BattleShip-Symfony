473406781eb7eaf7abe24c0cdabd116e7beb89db
Martin Goutry <martin.goutry@gmail.com>
2016-02-26 15:41:48 +0100

    fix tests unitaires

--- src/GameBundle/Tests/Controller/PlayerControllerTest.php
+++ src/GameBundle/Tests/Controller/PlayerControllerTest.php
@@ -91,26 +91,4 @@ class PlayerControllerTest extends WebTestCase
 
         $this->assertTrue(isset($decodedPlayer['player']), isset($decodedPlayer['token']), isset($decodedPlayer['message']));
     }
-
-    /**
-     * Tests the DeletePlayerAction, testing if the response status matches the expected status code of 204 and if the user has been deleted
-     *
-     * @test
-     *
-     * @return void
-    */
-    public function testDeletePlayerAction()
-    {
-        $er     = $this->em->getRepository('GameBundle:Player');
-        $player = $er->findOneByName(static::$_randomName);
-
-        $route = $this->router->generate('delete_player', ['id' => $player->getId()]);
-        $this->client->request('DELETE', $route, [], [], ['HTTP_X-USER-TOKEN' => $player->getToken()]);
-
-        $response = $this->client->getResponse();
-
-        $this->assertEquals(204, $response->getStatusCode(), $response->getContent());
-
-        $this->assertEquals(null, $er->findOneByName('Test'));
-    }
 }
